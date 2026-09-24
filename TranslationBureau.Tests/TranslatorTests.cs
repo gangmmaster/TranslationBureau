4eaf44fac8958a25fd71c593646e2be3bf4413e1
Шинкевич Кирилл <sinkevickirill3@gmail.com>
2026-09-24 11:02:34 +0300

    Проверка обнаружения ошибки конвейером

--- TranslationBureau.Tests/TranslatorTests.cs
+++ TranslationBureau.Tests/TranslatorTests.cs
@@ -1,10 +1,46 @@
-﻿namespace TranslationBureau.Tests;
+﻿using TranslationBureau.Domain.Entities;
+using TranslationBureau.Domain.Exceptions;
+using Xunit;
 
-public class UnitTest1
+namespace TranslationBureau.Tests;
+
+public class TranslatorTests
 {
     [Fact]
-    public void Test1()
+    public void Create_ПриКорректныхДанных_СоздаётАктивногоПереводчика()
     {
+        var translator = Translator.Create("Иванова Мария Петровна",
+            "+375291234567", "ivanova@example.com", "высшая", 22m);
 
+        Assert.Equal("Иванова Мария Петровна", translator.FullName);
+        Assert.Equal(25m, translator.RatePerUnit);
+        Assert.True(translator.IsActive);
+    }
+
+    [Fact]
+    public void Create_ПриПустомЗначенииФИО_ВозбуждаетИсключение()
+    {
+        Assert.Throws<DomainException>(() =>
+            Translator.Create("   ", null, null, "первая", 18m));
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void Create_ПриНеположительнойСтавке_ВозбуждаетИсключение(decimal rate)
+    {
+        Assert.Throws<DomainException>(() =>
+            Translator.Create("Петров Сергей Николаевич", null, null, "первая", rate));
+    }
+
+    [Fact]
+    public void Deactivate_ПереводитЗаписьВАрхивноеСостояние()
+    {
+        var translator = Translator.Create("Сидорова Анна Игоревна",
+            null, null, "вторая", 15m);
+
+        translator.Deactivate();
+
+        Assert.False(translator.IsActive);
     }
 }
